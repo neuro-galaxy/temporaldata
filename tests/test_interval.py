41c4c8a3ff4fadc2202e083fd1c707371c57b5e6
Mehdi Azabou <mehdiazabou@gmail.com>
2025-03-13 18:04:27 -0400

    update CHANGELOG and add test

--- tests/test_interval.py
+++ tests/test_interval.py
@@ -227,6 +227,11 @@ def test_or():
     Iexp = Interval.from_list([(0.0, 4.0), (10.0, 14.0)])
     easy_symmetric_check(I1, I2, Iexp, op)
 
+    I1 = Interval.from_list([(0.0, 1.0), (1.0, 2.0), (10.0, 11.0), (11.0, 12.0)])
+    I2 = Interval.from_list([(2.0, 3.0), (3.0, 4.0), (12.0, 13.0), (13.0, 14.0)])
+    Iexp = Interval.from_list([(0.0, 4.0), (10.0, 14.0)])
+    easy_symmetric_check(I1, I2, Iexp, op)
+
 
 def test_difference():
     op = lambda x, y: x.difference(y)
