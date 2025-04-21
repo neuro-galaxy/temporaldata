d3c11432ec6a2c7ba8711620bf757c6684b980be
AlexandreAndr <alexandreandre02018@gmail.com>
2025-04-21 00:20:33 -0400

    update some test_case

--- tests/test_add_split_mask.py
+++ tests/test_add_split_mask.py
@@ -1,20 +1,22 @@
-import pytest
-import os
 import copy
+import os
+import tempfile
+
 import h5py
 import numpy as np
 import pandas as pd
-import tempfile
+import pytest
+
 from temporaldata import (
     ArrayDict,
-    LazyArrayDict,
-    IrregularTimeSeries,
-    LazyIrregularTimeSeries,
-    RegularTimeSeries,
-    LazyRegularTimeSeries,
-    Interval,
-    LazyInterval,
     Data,
+    Interval,
+    IrregularTimeSeries,
+    LazyArrayDict,
+    LazyInterval,
+    LazyIrregularTimeSeries,
+    LazyRegularTimeSeries,
+    RegularTimeSeries,
 )
 
 
@@ -31,9 +33,9 @@ def test_regulartimeseries():
     data.add_split_mask("valid", valid_domain)
     data.add_split_mask("test", test_domain)
 
-    assert data.train_mask.sum() == 60
-    assert data.valid_mask.sum() == 20
-    assert data.test_mask.sum() == 20
+    assert data.train_mask.sum() == data.train_mask[:60].sum() == 60
+    assert data.valid_mask.sum() == data.valid_mask[60:80].sum() == 20
+    assert data.test_mask.sum() == data.test_mask[80:].sum() == 20
 
     # check what happens when things are skewed
     data = RegularTimeSeries(
@@ -48,9 +50,9 @@ def test_regulartimeseries():
     data.add_split_mask("valid", valid_domain)
     data.add_split_mask("test", test_domain)
 
-    assert data.train_mask.sum() == 60
-    assert data.valid_mask.sum() == 18
-    assert data.test_mask.sum() == 20
+    assert data.train_mask.sum() == data.train_mask[:60].sum() == 60
+    assert data.valid_mask.sum() == data.valid_mask[61:79].sum() == 18
+    assert data.test_mask.sum() == data.test_mask[80:].sum() == 20
 
 
 def test_set_split_domain():
@@ -65,6 +67,6 @@ def test_set_split_domain():
     data.set_valid_domain(Interval(6.0, 8.0))
     data.set_test_domain(Interval(8.0, 10.0))
 
-    assert data.lfp.train_mask.sum() == 60
-    assert data.lfp.valid_mask.sum() == 20
-    assert data.lfp.test_mask.sum() == 20
+    assert data.lfp.train_mask.sum() == data.lfp.train_mask[:60].sum() == 60
+    assert data.lfp.valid_mask.sum() == data.lfp.valid_mask[60:80].sum() == 20
+    assert data.lfp.test_mask.sum() == data.lfp.test_mask[80:].sum() == 20
