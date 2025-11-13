c076b8c8ff407b20a207bd54e63181e79605656e
Oumayma Gharbi <oumayma.gharbi@umontreal.ca>
2025-11-13 18:19:37 -0500

    Update mne converter

---
+++ temporaldata/converters/__init__.py
@@ -0,0 +1,1 @@
+from .from_mne import raw_to_temporaldata
---
+++ temporaldata/converters/from_mne.py
@@ -0,0 +1,11 @@
+import mne
+
+from temporaldata import IrregularTimeSeries, RegularTimeSeries
+
+
+def raw_to_temporaldata(mne_raw):
+    """
+    Convert an MNE-Python raw obaject to a temporaldata objec
+    """
+
+    raise NotImplementedError
